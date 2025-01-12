b5df39769403a91f926590ac22a467dec17bef38
Dzulfikar Fauzi <dzulfikar68.fauzi@gmail.com>
2025-01-13 01:44:49 +0700

    update config

--- 3optionscomposition/my-app/src/components/CompositionComponent.vue
+++ 3optionscomposition/my-app/src/components/CompositionComponent.vue
@@ -1,16 +1,14 @@
 <template>
-    <button @click="showName">Show Composition</button>
-    <h2>{{ name }}</h2>
+  <button @click="showName">Show Composition</button>
+  <h2>{{ name }}</h2>
 </template>
 
 <script setup>
 const name = 'Composition Component'
 
 const showName = () => {
-    console.log(name)
+  console.log(name)
 }
 </script>
 
-<style scoped>
-    
-</style>
+<style scoped></style>
