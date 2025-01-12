30b3acdadaf4a2c56e413d756d015f3cef592303
Dzulfikar Fauzi <dzulfikar68.fauzi@gmail.com>
2025-01-13 01:25:29 +0700

    add function event handler

--- 3optionscomposition/my-app/src/components/CompositionComponent.vue
+++ 3optionscomposition/my-app/src/components/CompositionComponent.vue
@@ -1,9 +1,14 @@
 <template>
+    <button @click="showName">Show Composition</button>
     <h2>{{ name }}</h2>
 </template>
 
 <script setup>
 const name = 'Composition Component'
+
+const showName = () => {
+    console.log(name)
+}
 </script>
 
 <style scoped>
